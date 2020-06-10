9e56051b36346e3029409ef4282257095f5878f9
sjadriana <adriana.sj@live.com>
2020-06-10 12:02:46 -0300

    funções

--- public/pages/feed/data.js
+++ public/pages/feed/data.js
@@ -11,3 +11,28 @@ export const logout = () => {
       // An error happened.
     });
 }
+export const createPost = (text) => {
+  firebase.firestore().collection("post").add({
+    text: text,
+    likes: 0,
+    name: "Adriana",
+    date: new Date().toLocaleString('pt-BR')
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  }); 
+}
+
+export const readPosts = (callback) => {
+  firebase.firestore().collection("post")
+    .onSnapshot(function(querySnapshot) {
+        var posts = [];
+        querySnapshot.forEach(function(doc) {
+            posts.push(doc.data());
+        });
+        callback(posts)
+    });
+}
--- public/pages/feed/main.js
+++ public/pages/feed/main.js
@@ -1,4 +1,4 @@
-import {logout}  from './data.js'
+import {logout, createPost, readPosts }  from './data.js'
 
 export const feed = () => {
   const container = document.createElement('div');
@@ -11,17 +11,33 @@ export const feed = () => {
       <button id='post-btn' type='submit' class="feed-btn-postar">Postar</button>
       <button id='logout-btn' class="feed-btn-logout">Logout</button>
     </form>
+    <div id='postar'>olaaa</div>
+    <div id='date' class=''date></div>
 `;
 
   const logoutBtn = container.querySelector('#logout-btn');
   const postBtn = container.querySelector('#post-btn');
+  const post = container.querySelector('#post');
+  const areaposts = container.querySelector('#postar');
+  const data = container.querySelector('#date');
+  const now = new Date;
+  //const dayName = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado");
 
   logoutBtn.addEventListener('click', (event) =>{
     event.preventDefault();
     logout();
   });
 
-
+  postBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    createPost(post.value);
+    areaposts .innerHTML = "";
+    readPosts(postTemplate);
+    
+  });
+  const postTemplate = (array) => {
+    areaposts.innerHTML = array.map(post => `<p>${post.text}</p>`).join("");
+      }   
   return container;
 };
 
