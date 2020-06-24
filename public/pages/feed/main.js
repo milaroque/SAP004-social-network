318fe9ed30c57c2ac482a5728d53f35f16d08bac
Camila Roque <milaroque@icloud.com>
2020-06-24 10:27:09 -0300

    teste css

--- public/pages/feed/main.js
+++ public/pages/feed/main.js
@@ -5,7 +5,8 @@ import {
 
 export const feed = () => {
   const container = document.createElement('div');
-  container.classList.add('main-feed')
+  document.body.className = 'main-feed';
+  
   container.innerHTML = ` 
   <div class='fundo'>
   <div class='navbar'>
@@ -43,79 +44,84 @@ export const feed = () => {
   const inputPost = container.querySelector('#post-input');
   const form = container.querySelector('#form');
   const profile = container.querySelector('#profile');
-
+  postBtn.disabled = true;
   const templateProfile = (user) => {
     profile.innerHTML = '';
-      const userProfile = document.createElement('div');
+    const userProfile = document.createElement('div');
       userProfile.innerHTML = `
-    <fieldset class='textarea-perfil'>
     <div class='img-perfil'>
-    <img id='img-perfil' data-id=${user.id} src=${user.photoURL}>
-    <div class='btn-profile'><strong>Editar Foto</strong></div>
-    <input id='foto-perfil' type='file'>
+      <figure class='img-profile'> 
+        <img id='img-perfil' class='img-profile' data-id=${user.id} src=${user.photoURL}>
+      </figure>   
+      <div>
+        <textarea id='first-name' class='personal-info' data-id= ${user.id} type='text' disabled>${user.name}</textarea>
+        <div class='textarea-location'>Localização: 
+          <img class='size pin' src='../../assets/pin.png'> <textarea id='location' class='textarea-location' type='text' data-id=${user.id} disabled>${user.location}</textarea>
+        </div>          
+      </div>
     </div>
-  </fieldset>
-  <fieldset class='textarea-perfil'>
-  <div>Nome: <textarea id='first-name' class='personal-info' data-id= ${user.id} type='text' disabled>${user.name}</textarea></div>
-  </fieldset>
-  <fieldset class='textarea-location'>
-  <div class='textarea-location'>Localização: <img class='size' src='../../assets/pin.png'>
-  <textarea id='location' class='personal-info' type='text' data-id=${user.id} disabled>${user.location}</textarea></div>  
-  </fieldset>
+  <div class='edit-btn'>
+  <label for='foto-perfil' class='btn-profile'><strong>Editar Foto</strong>
+  <input id='foto-perfil' type='file'>
+  </label>
   <div class='bnt-spaceProfile'>
-    <button id='edit-btn' class='edit size' data-id= ${user.id}>
-      <img class='save size' src='../../assets/edit.png'>
+    <button id='edit-btn' class='edit-profile' data-id= ${user.id}>
+      <img class='edit-profile' src='../../assets/edit.png'>
     </button>
-    <button id='save-btn' class='save size' data-id= ${user.id}>
-      <img class='save size' src='../../assets/tick.png'>
+    <button id='save-btn' class='edit-profile' data-id= ${user.id}>
+      <img class='edit-profile' src='../../assets/tick.png'>
     </button>
+    </div>
   </div>
   `
-      profile.appendChild(userProfile)
+    profile.appendChild(userProfile)
 
-      const textName = userProfile.querySelector('#first-name');
-      const textLocation = userProfile.querySelector('#location');
-      const inputImg = userProfile.querySelector('#foto-perfil');
-      const divImg = userProfile.querySelector('#img-perfil');
-      const editProfileBtn = userProfile.querySelector('#edit-btn');
-      const saveEditedProfileBtn = userProfile.querySelector('#save-btn');
+    const textName = userProfile.querySelector('#first-name');
+    const textLocation = userProfile.querySelector('#location');
+    const inputImg = userProfile.querySelector('#foto-perfil');
+    const divImg = userProfile.querySelector('#img-perfil');
+    const editProfileBtn = userProfile.querySelector('#edit-btn');
+    const saveEditedProfileBtn = userProfile.querySelector('#save-btn');
 
-      inputImg.onchange = function (event) {
-        printImg(event, user.id, divImagem, divImg)
-      }
-      const divImagem = (divImg, url) => {
-        return divImg.innerHTML += `<img src ='${url}'>`
-      }
-
-      editProfileBtn.addEventListener('click', (event) => {
-        event.preventDefault();
-        textName.disabled = false;
-        textLocation.disabled = false;
-        return editProfileBtn.dataset.id
-      })
-      saveEditedProfileBtn.addEventListener('click', (event) => {
-        event.preventDefault();
-        textName.disabled = true;
-        textLocation.disabled = true;
-        updateProfile(saveEditedProfileBtn.dataset.id, textName, textLocation)
-      })
+    inputImg.onchange = function (event) {
+      printImg(event, user.id, divImagem, divImg)
+    }
+    const divImagem = (divImg, url) => {
+      return divImg.innerHTML += `<img src ='${url}'>`
+    }
+    editProfileBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      textName.disabled = false;
+      textLocation.disabled = false;
+      return editProfileBtn.dataset.id
+    })
+    saveEditedProfileBtn.addEventListener('click', (event) => {
+      event.preventDefault();
+      textName.disabled = true;
+      textLocation.disabled = true;
+      updateProfile(saveEditedProfileBtn.dataset.id, textName, textLocation)
+    })
   }
-  
+
   logoutBtn.addEventListener('click', (event) => {
     event.preventDefault();
     logout();
   });
 
-  postBtn.addEventListener('click', (event, post) => {
+  inputPost.onchange = function () {
+    if (inputPost.value !== '') {
+      postBtn.disabled = false;
+    } else {
+      postBtn.disabled = true;
+    }
+  }
+
+  postBtn.addEventListener('click', (event) => {
     event.preventDefault();
-    if (inputPost.value === ''){
-      console.log('campo vazio')
-    }else{
     createPost(inputPost.value, form.privacy.value);
     allPosts.innerHTML = '';
     timeline(templatePost);
     inputPost.value = '';
-    }
   });
 
   const templatePost = (arrayPosts) => {
@@ -127,7 +133,7 @@ export const feed = () => {
       <div class='postedfeed'>
       <p id ='datePost'class='posted-for'>${post.user}, em ${post.date}
       </p>
-    <form id='privacy' class='privacy'>
+    <form id='privacy' style="display: none;" class='privacy'>
       <input type="radio" name="privacy" id="public" class="btn-privacy" value="public" checked><img class='public btn-privacy' src='../../assets/public.png'>
     <input type="radio" name="privacy" id="private" class="btn-privacy" value="private"><img class='private btn-privacy' src='../../assets/private.png'>
     </form>
@@ -135,17 +141,17 @@ export const feed = () => {
     <textarea id='text-area' data-id=${post.id} class='post' disabled>${post.text}</textarea>
     <div class='bnt-space'>
     <button id='like-btn' class='likes-btn size' data-id= ${post.id}>
-    <img class='likes size' src='../../assets/001-paw.png' width='20'>${post.likes}</button>'
+    <img class='likes size' src='../../assets/001-paw.png' width='20'>${post.likes}</button>
     <button id='comment-btn' class ='comment-btn' data-id= ${post.id}><img class='likes size' src='../../assets/comment.png' width='20'></button> <br>
     <button id='edit-btn' class='edit size' data-id= ${post.id}><img class='save size' src='../../assets/edit.png'></button>
-    <button id='save-btn' class='save size' data-id= ${post.id}><img class='save size' src='../../assets/tick.png'></button>
+    <button id='save-btn' style="display: none;" class='save size' data-id= ${post.id}><img class='save size' src='../../assets/tick.png'></button>
     </div>
     <div class='comments-area' id='all-comments' style="display: none;">
     <input class='comment-input' placeholder=' Digite seu comentário' type='text'>
     <button class='commentBtn' id='btnComment' type='submit' >Comentar</button>
     </div>
     </div>
-    <div id='commented' class='commented2'>
+    <div id='commented' class='commented2' style="display: none;">${post.comments}
     </div>`
 
       allPosts.appendChild(template);
@@ -162,8 +168,16 @@ export const feed = () => {
       const saveBtn = template.querySelector('#save-btn');
       const privacyBtn = template.querySelector('#privacy')
       const datePost = template.querySelector('#datePost')
-      saveBtn.style.display = 'none';
-      privacyBtn.style.display = 'none';
+ 
+      commentButton.disabled = true;
+
+      inputComments.onchange = function () {
+        if (inputComments.value !== '') {
+          commentButton.disabled = false;
+        } else {
+          commentButton.disabled = true;
+        }
+      }
 
       if (post.user_uid === firebase.auth().currentUser.uid) {
         deleteBtn.addEventListener('click', (event) => {
@@ -193,6 +207,7 @@ export const feed = () => {
         deleteBtn.style.display = 'none';
         privacyBtn.style.display = 'none';
         datePost.style.display = 'flex'
+
         likeBtn.addEventListener('click', (event) => {
           event.preventDefault();
           likePost(likeBtn.dataset.id);
@@ -209,7 +224,7 @@ export const feed = () => {
         <textarea id='text-area' class='comment-area' data-id=${comment.id} disabled>${comment.text}</textarea>
         <div class='btn-comment'>
         <button id='edit-comment' class='edit size' data-id= ${comment.id}><img class='save size' src='../../assets/edit.png'></button>
-      <button id='save-comment' class='save size' data-id= ${comment.id}><img class='save size' src='../../assets/tick.png'></button>
+      <button id='save-comment' style="display: none;" class='save size' data-id= ${comment.id}><img class='save size' src='../../assets/tick.png'></button>
       </div>
         </div>
         `
@@ -219,6 +234,7 @@ export const feed = () => {
           const editComments = containerComment.querySelector('#edit-comment');
           const saveComment = containerComment.querySelector('#save-comment');
 
+          if (comment.user_uid === firebase.auth().currentUser.uid) {
           deleteComment.addEventListener('click', (event) => {
             event.preventDefault();
             deleteComments(post.id, deleteComment.dataset.id);
@@ -235,34 +251,44 @@ export const feed = () => {
             editComment(editComments.dataset.id);
             saveComment.style.display = 'flex';
           })
-
           saveComment.addEventListener('click', (event) => {
             event.preventDefault();
             const textComment = containerComment.querySelector('#text-area');
             textComment.disabled = true;
             saveEditedComment(post.id, saveComment.dataset.id, textComment)
           })
+        }else {
+        editComments.style.display = 'none';
+        deleteComment.style.display = 'none';
+        }
         })
       }
 
       commentButton.addEventListener('click', (event) => {
         event.preventDefault();
-        if (inputComments.value === ''){
-          console.log('campo vazio')
-        }else{
-        createComment(inputComments.value, post.id);
-        allComments.innerHTML = '';
-        readComment(post.id, templateComment);
-        inputComments.value = '';
-        }
+          createComment(inputComments.value, post.id);
+          allComments.innerHTML = '';
+          readComment(post.id, templateComment);
+          inputComments.value = '';
       });
 
       commentBtn.addEventListener('click', (event) => {
         event.preventDefault();
-        template.querySelector('.comments-area').style.display = 'flex';
-        readComment(post.id, templateComment)
+       toHideDivs('#all-comments', '#commented')
       })
 
+      function toHideDivs(divPost, divCom) {
+        let displayPost = template.querySelector(divPost).style.display;
+        let displayComment = template.querySelector(divCom).style.display;
+        if(displayPost == "none"){
+          template.querySelector(divPost).style.display = 'flex';
+          template.querySelector(divCom).style.display = 'block';
+            readComment(post.id, templateComment)
+      }else if (displayPost == "flex" || displayComment == 'block') {
+        template.querySelector(divPost).style.display = 'none';
+        template.querySelector(divCom).style.display = 'none';
+    }
+  }
     }).join('');
   };
   timeline(templatePost);
